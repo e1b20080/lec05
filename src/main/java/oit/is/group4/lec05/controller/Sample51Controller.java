b9a244474cb49252faba683c144cfcb95e027ddb
e1b20108 <e1b20108@oit.ac.jp>
2022-10-25 16:08:20 +0900

    フルーツの表示

--- src/main/java/oit/is/group4/lec05/controller/Sample51Controller.java
+++ src/main/java/oit/is/group4/lec05/controller/Sample51Controller.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-// import oit.is.group4.lec05.model.Fruit;
-// import oit.is.group4.lec05.model.FruitMapper;
+import oit.is.group4.lec05.model.Fruit;
+import oit.is.group4.lec05.model.FruitMapper;
 
 /**
  * /sample5へのリクエストを扱うクラス authenticateの設定をしていれば， /sample5へのアクセスはすべて認証が必要になる
@@ -21,12 +21,35 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/sample5")
 public class Sample51Controller {
 
-  // @Autowired
-  // FruitMapper fMapper;
+  @Autowired
+  FruitMapper fMapper;
 
   @GetMapping("step1")
   public String sample51() {
     return "sample51.html";
   }
+
+  @GetMapping("step2")
+  public String sample52(ModelMap model) {
+    ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+    model.addAttribute("fruits2", fruits2);
+    return "sample51.html";
+  }
+
+  // @GetMapping("step3")
+  // @Transactional
+  // public String sample53(@RequestParam Integer id, ModelMap model) {
+  //   // 削除対象のフルーツを取得
+  //   Fruit fruit3 = fMapper.selectById(id);
+  //   model.addAttribute("fruit3", fruit3);
+
+  //   // 削除
+  //   fMapper.deleteById(id);
+
+  //   // 削除後のフルーツリストを取得
+  //   ArrayList<Fruit> fruits2 = fMapper.selectAllFruit();
+  //   model.addAttribute("fruits2", fruits2);
+  //   return "sample51.html";
+  // }
 
 }
